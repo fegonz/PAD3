f42368dc2562b5fe9f5764e631d88f66251e6e01
fegonz08 <fegonz08@ucm.es>
2021-06-04 13:41:56 +0200

    SW terminado pero deshabilitado

--- sw.js
+++ sw.js
@@ -1,107 +1,96 @@
+ACHE_NAME = 'cache-1';
+
+const CACHE_STATIC_NAME = 'static-v1';
+const CACHE_DYNAMIC_NAME = 'dynamic-v1';
+const CACHE_INMUTABLE_NAME = 'inmutable-v1';
+
+
+
+function limpiarCache(cacheName, numeroItems) {
+    caches.open(cacheName).then(cache => {
+        return cache.keys().then(keys => {
+            if (keys.length > numeroItems) {
+                cache.delete(keys[0]).then(limpiarCache(cacheName, numeroItems));
+            }
+        });
+    })
+
+
+}
+
+
+
 self.addEventListener('install', e => {
 
-    const cacheProm = caches.open('cache-1').then(cache => {
+    const cacheProm = caches.open(CACHE_STATIC_NAME).then(cache => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/css/style.css',
+                '/js/app.js'
+
+
+            ])
+        })
+        //para agnadir librerias de bootstrap
+    const cacheInmutable = caches.open(CACHE_INMUTABLE_NAME).then(cache => {
         return cache.addAll([
-            '/',
-            '/index.html',
-            '/js/app.js',
-            '/css/style.css'
 
 
 
         ])
     })
-    e.waitUntil(cacheProm);
+
+    e.waitUntil(Promise.all([cacheProm, cacheInmutable]));
 });
+
+
+self.addEventListener('activate', e => {
+
+    const activacion = caches.keys().then(keys => {
+
+        keys.forEach(key => {
+            if (key !== CACHE_STATIC_NAME && key.includes('static')) {
+                return caches.delete(key);
+            }
+        })
+    })
+
+    e.waitUntil(activacion);
+
+})
 
 
 self.addEventListener('fetch', e => {
 
 
 
-    //2-Cache with Network Fallback
-    //caches.match(e.request);
-
-
-
-
-    //e.respondWith();
-
-    //1- Cache Only
+    //Cache with Network Fallback then Cache
     //el match se va a todos los caches que esten en este dominio y va a buscar y retornar uno que
     //coincida con el e.request
-    e.respondWith(caches.match(e.request));
+    const respuesta = caches.match(e.request).then(res => {
+
+        if (res) return res;
+
+        //no existe el archivo que esta pidiendo
+        //tengo que ir a la web
+
+        console.log('No existe', e.request.url);
+
+        return fetch(e.request).then(newResp => {
+
+            caches.open(CACHE_DYNAMIC_NAME).then(cache => {
+                cache.put(e.request, newResp);
+                limpiarCache(CACHE_DYNAMIC_NAME, 50);
+            });
+            return newResp.clone();
+        });
+    });
+
+
+
+
+    e.respondWith(respuesta);
+
+
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* const offlineResp = new Response(`
-Bienvenido a mi pagina Web
-
-Disculpa, para usarla necesitas Internet
-    
-    
-`); */
-
-/* 
-    const offlineResp = new Response(`
-    <!DOCTYPE html>
-        <html lang="en">
-
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            <title>Mi PWA</title>
-
-           
-        </head>
-
-        <body class="container p-3">
-
-
-        <h1>Offline Mode</h1>
-
-        </body>
-        </html>
-    
-    `, {
-        headers: {
-            'Content-Type': 'text/html'
-        }
-    }); */
-
-
-
-
-
-
-
-
-
-
-
-/* 
-el request es lo que se está solicitando, puede ser que el archivo no exista
-Para cada peticion fetch va devolver lo mismo que se esta pidiendo
-self.addEventListener('fetch', event =>{
-    event.respondWith(fetch(event.request));
-}); */
